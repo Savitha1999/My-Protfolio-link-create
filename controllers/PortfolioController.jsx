924bf1f1f8a391693955d518f7d0254998fcf685
hsavitha1999 <hsavitha1999@gmail.com>
2025-07-09 23:15:57 +0530

    Add_Portfolio

---
+++ controllers/PortfolioController.jsx
@@ -0,0 +1,27 @@
+// const sendEmailController =(req,res ) => {
+//     try
+//     {
+//          return res.status(200).send({
+//             success:true,
+//             message:'Your Message Send SuccessFully ',
+//          });
+//     }    catch (error)
+//         {
+//              console.log(error)
+//              return res.status(500).send({
+//                 success:false,
+//                 message:'Send Email API Error',
+//                 error,
+//              })
+//         }
+    
+// };
+
+// module.exports = {sendEmailController};
+
+
+
+
+
+
+
